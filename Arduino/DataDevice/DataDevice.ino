c80030f7d1fd042f4c86761e5ac1b4994d1cb43b
Matthew Whited <matt@whited.us>
2022-12-27 22:58:44 -0500

    update examples

--- Arduino/DataDevice/DataDevice.ino
+++ Arduino/DataDevice/DataDevice.ino
@@ -1,12 +1,9 @@
-
-// https://www.osepp.com/electronic-modules/shields/45-16-2-lcd-display-keypad-shield
-
-void setup() {
-  // put your setup code here, to run once:
-
+void setup() 
+{
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop() 
+{
 }
+
+
